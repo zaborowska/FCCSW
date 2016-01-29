50a9c9a12a2ea5e324b954df96b75b51f86eb2b4
Valentin Volkl <valentin.volkl@cern.ch>
2016-01-29 13:19:09 +0100

    Finish example of an algorithm having multiple instances of a private tool.

--- Sim/SimTests/options/multipletoolinstancesalg.py
+++ Sim/SimTests/options/multipletoolinstancesalg.py
@@ -1,9 +1,12 @@
 from Gaudi.Configuration import *
-from Configurables import ApplicationMgr, FCCDataSvc, ToolWithOutput, ToolWithTool, TestToolAlgorithm, AlbersOutput, TestMultipleToolInstancesAlgorithm
+from Configurables import ApplicationMgr, FCCDataSvc, ToolWithOutput, ToolWithTool, TestToolAlgorithm, AlbersOutput, TestMultipleToolInstancesAlgorithm, ToolWithMember
 
 albersevent = FCCDataSvc("EventDataSvc")
 
-alg =  TestMultipleToolInstancesAlgorithm("alg")
+
+#tool1 = ToolWithMember("tool1")
+#tool2 = ToolWithMember("tool2")
+alg =  TestMultipleToolInstancesAlgorithm("alg")#, tool1=tool1, tool2=tool2)
 
 out = AlbersOutput("out",
                    OutputLevel=DEBUG)

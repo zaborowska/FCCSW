6ef188626f82a48cc7e7931149441240f2c12d25
zaborowska <azaborow@cern.ch>
2016-11-15 16:42:49 +0100

    Save ID in debug tool saving positioned cell

--- Reconstruction/RecCalorimeter/src/components/CreatePositionedHit.cpp
+++ Reconstruction/RecCalorimeter/src/components/CreatePositionedHit.cpp
@@ -72,22 +72,17 @@ StatusCode CreatePositionedHit::execute() {
   auto edmPositionedHitCollection = m_caloPositionedHits.createAndPut();
 
   //TODO: read hardcoded numbers from config file
-  double rCellMin = 270.;
+  double rCellMin = 2700.;
   int nActiveLayers[3]={22,83,11};
   auto decoder = m_geoSvc->lcdd()->readout(m_readoutName).idSpec().decoder();
   //Loop though CaloHits, calculate position from cellID, create and fill information in a CaloCluster
-  for (const auto& ecells : *calocells) {
-    auto positionedHit = edmPositionedHitCollection->create();
-    positionedHit.core().energy = ecells.core().energy;
-    positionedHit.core().time = ecells.core().time;
-    positionedHit.core().bits = ecells.core().cellId;
-
+  for (const auto& cell : *calocells) {
     //Current active layer r-minimum r
     //Volume dimensions of Tube - det::utils::tubeDimensions(volumeID) - ThreeVector rmin, rmax, dz (half-length)
-    double rmin_layer = det::utils::tubeDimensions(ecells.core().cellId).x();
+    double rmin_layer = det::utils::tubeDimensions(cell.core().cellId).x();
     //Next active layer [ needed for r_cell = middle of the cell (active + passive) ]
     //If half size of cell in r not known, calculate it from the next layer r-min
-    decoder->setValue(ecells.core().cellId);
+    decoder->setValue(cell.core().cellId);
     int layer = (*decoder)[m_activeFieldName].value();
     if (layer>=m_numLayers) {
       error()<<"More layers than expected!!!"<<endmsg;
@@ -114,15 +109,18 @@ StatusCode CreatePositionedHit::execute() {
 
 
     //Global position of the cell
-    auto position =  m_segmentation->positionFromREtaPhi( rCell, m_segmentation->eta(ecells.core().cellId), m_segmentation->phi(ecells.core().cellId) );
-    positionedHit.position().x = position.x()*10.;
-    positionedHit.position().y = position.y()*10.;
-    positionedHit.position().z = position.z()*10.;
+    auto position =  m_segmentation->positionFromREtaPhi( rCell, m_segmentation->eta(cell.core().cellId), m_segmentation->phi(cell.core().cellId) );
+    auto edmPos = fcc::Point();
+    edmPos.x = position.x();
+    edmPos.y = position.y();
+    edmPos.z = position.z();
+
+    auto positionedHit = edmPositionedHitCollection->create(edmPos, cell.core());
 
     //Debug information about cells
-    debug() << "cellID " << ecells.core().cellId <<" energy " << ecells.core().energy << " decoder: all fields "
-            << decoder->valueString() << " r " << rCell << " eta " <<  m_segmentation->eta(ecells.core().cellId)
-            << " phi " <<  m_segmentation->phi(ecells.core().cellId)<< endmsg;
+    debug() << "cellID " << cell.core().cellId <<" energy " << cell.core().energy << " decoder: all fields "
+            << decoder->valueString() << " r " << rCell << " eta " <<  m_segmentation->eta(cell.core().cellId)
+            << " phi " <<  m_segmentation->phi(cell.core().cellId)<< endmsg;
   }
   debug() << "Output CaloCluster collection size: " << edmPositionedHitCollection->size() << endmsg;
 
